0183f4e34f8ef65f21fb7f2bbc5550721f726460
ankit <69717009+crew-guy@users.noreply.github.com>
2021-09-17 05:50:28 +0530

    latency minimized

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios')
 const express = require('express')
 const redis = require('redis')
 const util = require('util')
@@ -21,6 +22,21 @@ app.get('/', async (req, res) => {
     const {key} = req.body
     const response = await client.get(key)
     res.json(response)
+})
+
+
+app.get('/posts/:id', async (req, res) => {
+    const {id} = req.body
+    const cachedPost = await client.get(`post-${id}`)
+
+    if (cachedPost) {
+        return res.json(JSON.parse(cachedPost))
+    }
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const cachedRes = await client.set(`post-${id}`, JSON.stringify(response.data))
+    console.log(cachedRes)
+    return res.json(response.data);
+
 })
 
 app.listen(8080, () => {
